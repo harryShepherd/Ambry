29ad88e47f665d9b8248edf91a34710d36e9e3ee
Harry <harryshepherd0.0@hotmail.com>
2024-02-05 17:11:14 +0000

    Changed bridge name

--- src/main/java/com/napier/Ambry/App.java
+++ src/main/java/com/napier/Ambry/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main(String[] args)
     {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("Ambry-Bridge");
+        MongoClient mongoClient = new MongoClient("ambry-bridge");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
