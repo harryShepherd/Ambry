277cffaa92061230b085c3e8c0d94e31ee63ac76
Harry <harryshepherd0.0@hotmail.com>
2024-02-08 14:42:10 +0000

    Removed main method from App.java

--- src/main/java/com/napier/Ambry/Main.java
+++ src/main/java/com/napier/Ambry/Main.java
@@ -3,6 +3,13 @@ package com.napier.Ambry;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        // a.disconnect();
     }
 }
