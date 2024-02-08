15739169cb82cb7007d7f1db5d3e297a4e80e3a6
Harry <harryshepherd0.0@hotmail.com>
2024-02-08 14:44:12 +0000

    Updated Main.java to disconnect from DB

--- src/main/java/com/napier/Ambry/Main.java
+++ src/main/java/com/napier/Ambry/Main.java
@@ -3,13 +3,13 @@ package com.napier.Ambry;
 public class Main {
     public static void main(String[] args)
     {
-        // Create new Application
+        // Create new Application Instance
         App a = new App();
 
         // Connect to database
         a.connect();
 
         // Disconnect from database
-        // a.disconnect();
+        a.disconnect();
     }
 }
