dbe6f44e3019a729683515d5cc444845d0951162
Murdo Wallace <40591751@live.napier.ac.uk>
2024-02-13 14:31:03 +0000

    created Country.java to store information form world.sql

---
+++ src/main/java/com/napier/Ambry/Country.java
@@ -0,0 +1,152 @@
+/**
+ * Filename:    Country.java
+ * Author:      Murdo Wallace
+ * Last Edited: 13/2/2024 @ 14:28
+ * Purpose:     This class exists to store information about the country item
+ *              found in world.sql
+ */
+
+package com.napier.Ambry;
+
+public class Country {
+    private String code;
+    private String Name;
+    private String Continent;
+    private String Region;
+    private String SurfaceArea;
+    private String IndepYear;
+    private String Population;
+    private String LifeExpectancy;
+    private String GNP;
+    private String GNPOld;
+    private String LocalName;
+    private String GovernmentForm;
+    private String HeadOfState;
+    private String Capital;
+    private String Code2;
+
+    // Getter and setter for code
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String newCode) {
+        this.code = newCode;
+    }
+
+    // Getter and setter for name
+    public String getName() {
+        return Name;
+    }
+    public void setName(String newName) {
+        this.Name = newName;
+    }
+
+    // Getter and setter for Continent
+    public String getContinent() {
+        return Continent;
+    }
+    public void setnewContinent(String newContinent) {
+        this.Continent = newContinent;
+    }
+
+    // Getter and setter for Region
+    public String getRegion() {
+        return Region;
+    }
+    public void setRegion(String newRegion) {
+        this.Region = newRegion;
+    }
+
+    // Getter and setter for SurfaceArea
+    public String getSurfaceArea() {
+        return SurfaceArea;
+    }
+    public void setSurfaceArea(String newSurfaceArea) {
+        this.SurfaceArea = newSurfaceArea;
+    }
+
+    // Getter and setter for IndepYear
+    public String getIndepYear() {
+        return IndepYear;
+    }
+    public void setIndepYear(String newIndepYear) {
+        this.IndepYear = newIndepYear;
+    }
+
+    // Getter and setter for Population
+    public String getPopulation() {
+        return Population;
+    }
+    public void setPopulation(String newPopulation) {
+        this.Population = newPopulation;
+    }
+
+    // Getter and setter for LifeExpectancy
+    public String getLifeExpectancy() {
+        return LifeExpectancy;
+    }
+    public void setLifeExpectancy(String newLifeExpectancy) {
+        this.LifeExpectancy = newLifeExpectancy;
+    }
+
+    // Getter and setter for GNP
+    public String getGNP() {
+        return GNP;
+    }
+    public void setGNP(String newGNP) {
+        this.GNP = newGNP;
+    }
+
+    // Getter and setter for GNPOld
+    public String getGNPOld() {
+        return GNPOld;
+    }
+    public void setGNPOld(String newGNPOld) {
+        this.GNPOld = newGNPOld;
+    }
+
+    // Getter and setter for LocalName
+    public String getLocalName() {
+        return LocalName;
+    }
+    public void setLocalName(String newLocalName) {
+        this.LocalName = newLocalName;
+    }
+
+    // Getter and setter for GovernmentForm
+    public String getGovernmentForm() {
+        return GovernmentForm;
+    }
+    public void setGovernmentForm(String newGovernmentForm) {
+        this.GovernmentForm = newGovernmentForm;
+    }
+
+    // Getter and setter for HeadOfState
+    public String getHeadOfState() {
+        return HeadOfState;
+    }
+    public void setHeadOfState(String newHeadOfState) {
+        this.HeadOfState = newHeadOfState;
+    }
+
+    // Getter and setter for Capital
+    public String getCapital() {
+        return Capital;
+    }
+    public void setCapital(String newCapital) {
+        this.Capital = newCapital;
+    }
+
+    // Getter and setter for Code2
+    public String getCode2() {
+        return Code2;
+    }
+    public void setCode2(String newCode2) {
+        this.Code2 = newCode2;
+    }
+
+
+
+
+
+}
