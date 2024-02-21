c64dcd1a5d1378740389bb93b64f8b62f899f81f
Harry <harryshepherd0.0@hotmail.com>
2024-02-21 17:43:12 +0000

    Added getAllCountries() to Database

--- src/main/java/com/napier/Ambry/Main.java
+++ src/main/java/com/napier/Ambry/Main.java
@@ -1,5 +1,7 @@
 package com.napier.Ambry;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args)
     {
@@ -8,6 +10,15 @@ public class Main {
 
         // Connect to database
         a.connect();
+
+        ArrayList<Country> countries = a.getAllCountries();
+
+        System.out.println("Size of countries list: ");
+        System.out.println(countries.size());
+
+        for(int i =0; i < countries.size(); i++) {
+            System.out.println(countries.get(i).getName());
+        }
 
         // Disconnect from database
         a.disconnect();
