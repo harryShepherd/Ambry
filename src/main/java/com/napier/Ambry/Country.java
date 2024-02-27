c2297e3c88acfa41c61a124323fa4bb38189b99c
Murdo Wallace <40591751@live.napier.ac.uk>
2024-02-27 13:33:28 +0000

    added comments and made the code run getAllCountryLargeToSmall and getAllCountryPerContinent in a neater form

--- src/main/java/com/napier/Ambry/Country.java
+++ src/main/java/com/napier/Ambry/Country.java
@@ -155,26 +155,30 @@ public class Country {
      * Murdo Wallace
      */
     public static ArrayList<Country> getAllCountryLargeToSmall() {
+        //connects to the database
         try {
             Statement stmt = Database.con.createStatement();
-
+            //sql statement to return All the countries in the world organised by largest population to smallest.
             String str_select =
                     "SELECT * FROM country ORDER BY population";
 
             ResultSet rset = stmt.executeQuery(str_select);
-
+            //arraylist to store sql output
             ArrayList<Country> countries = new ArrayList<Country>();
-
+            //while we still have a result
             while (rset.next()) {
+                // insert result into country instance
                 Country country = new Country();
                 country.setCode(rset.getString("country.Code"));
                 country.setName(rset.getString("country.Name"));
                 country.setContinent(rset.getString("country.Continent"));
                 country.setRegion(rset.getString("country.Region"));
+                // add result to arraylist
                 countries.add(country);
             }
             return countries;
 
+            //prints error message of we cannot connect to database
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed");
@@ -182,28 +186,36 @@ public class Country {
         }
 
     }
-
+    /**
+     * Epic1: All the countries in a continent organised by largest population to smallest.
+     * returns All the countries in a continent organised by largest population to smallest.
+     * Murdo Wallace / Harry Shepherd
+     */
     public static ArrayList<Country> getAllCountryPerContinent(String cont) {
+        //connects to the database
         try {
             Statement stmt = Database.con.createStatement();
-
+            //sql statement to return All the countries in a continent organised by largest population to smallest
             String str_select =
                     "SELECT * FROM country WHERE Continent='" + cont + "'ORDER BY population";
 
             ResultSet rset = stmt.executeQuery(str_select);
 
+            //arraylist to store sql output
             ArrayList<Country> countries = new ArrayList<Country>();
-
+            //while we still have a result
             while (rset.next()) {
+                // insert result into country instance
                 Country country = new Country();
                 country.setCode(rset.getString("country.Code"));
                 country.setName(rset.getString("country.Name"));
                 country.setContinent(rset.getString("country.Continent"));
                 country.setRegion(rset.getString("country.Region"));
+                // add result to arraylist
                 countries.add(country);
             }
             return countries;
-
+        //prints error message of we cannot connect to database
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed");
--- src/main/java/com/napier/Ambry/Main.java
+++ src/main/java/com/napier/Ambry/Main.java
@@ -9,15 +9,22 @@ public class Main {
 
         // Connect to database
         a.connect();
-
-       // ArrayList<Country> print = Country.getAllCountryLargeToSmall();
-        /*
+        //creating an arraylist to store the output of getAllCountryLargeToSmall
+       ArrayList<Country> print = Country.getAllCountryLargeToSmall();
+        //prints all of arraylist print
+        System.out.print("getAllCountryLargeToSmall: \n");
+        System.out.print("\n");
         for(int i = 0; i < print.size(); i++){
             System.out.print(print.get(i).getName() + "\n");
         }
-    */
-        ArrayList<Country> print2 = Country.getAllCountryPerContinent("Asia");
+        System.out.print("\n");
 
+        //creating an arraylist to store the output of getAllCountryPerContinent
+        //where the continent is asia
+        ArrayList<Country> print2 = Country.getAllCountryPerContinent("Asia");
+        System.out.print("getAllCountryPerContinent: \n");
+        System.out.print("\n");
+        //prints all of arraylist print2
         for(int i = 0; i < print2.size(); i++){
             System.out.print(print2.get(i).getName() + "\n");
         }
