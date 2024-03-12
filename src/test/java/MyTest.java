95bdb468b89a41c8532791d1a3e5f8c0601c2a52
Cameron <cameron.smith2814@gmail.com>
2024-03-12 12:27:57 +0000

    Added unit tests

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -6,4 +6,46 @@ public class MyTest {
     void unitTest() {
         assertEquals(5, 5);
     }
+
+    @Test
+    void unitCheckNull(){}
+
+    @Test
+    void unitCheckCountryStandard(){}
+
+    @Test
+    void unitCheckCityStandard(){}
+
+    @Test
+    void unitGetAllCountryLargeToSmall(){}
+
+    @Test
+    void unitGetAllCountryPerContinent(){}
+
+    @Test
+    void unitGetAllCountryPerRegion(){}
+
+    @Test
+    void unitTopNWorld(){}
+
+    @Test
+    void unitTopNContinent(){}
+
+    @Test
+    void unitTopNRegion(){}
+
+    @Test
+    void unitWorldCities(){}
+
+    @Test
+    void unitRegionCities(){}
+
+    @Test
+    void unitCountryCities(){}
+
+    @Test
+    void unitDistrictCities(){}
+
+    @Test
+    void unitContinentCity(){}
 }
