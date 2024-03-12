16e0e9d4a6f1b5cc82e3260fa4dc69e790ffd98d
Harry <harryshepherd0.0@hotmail.com>
2024-03-12 12:48:50 +0000

    Updated connect function, requires two parameters Updated unit testing

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,9 +1,18 @@
+import com.napier.Ambry.Database;
 import org.junit.jupiter.api.*;
+
+import static com.napier.Ambry.City.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTest {
+    @BeforeAll
+    static void init(){
+        Database db = new Database();
+
+        db.connect("localhost:33060", 30000);
+    }
     @Test
     void unitTest() {
-        assertEquals(5, 5);
+        assertEquals(null, CityStandard(""));
     }
 }
