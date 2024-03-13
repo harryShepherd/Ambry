03bfc1bdbbef83db1472599a5085bd83cc8b651f
Harry <harryshepherd0.0@hotmail.com>
2024-03-13 12:14:30 +0000

    Removed old connect function

--- src/main/java/com/napier/Ambry/Database.java
+++ src/main/java/com/napier/Ambry/Database.java
@@ -20,48 +20,6 @@ public class Database
     /**
      * Connect to the MySQL database.
      */
-
-    /*
-    public void connect()
-    {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-    }
-
-     */
-
     public void connect(String location, int delay) {
         try {
             // Load Database driver
